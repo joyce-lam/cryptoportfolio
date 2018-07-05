201faed1a9bab36112459e93da84dd110247c2b9
Joyce <joycelamsaungan@gmail.com>
2018-07-04 19:04:17 -0700

    fixing files for deployment

--- config/keys.js
+++ config/keys.js
@@ -2,3 +2,10 @@ exports.jwt = {
 	secret: process.env.JWT_SECRET,
 	alogirithm: process.env.JWT_ALGORITHM
 };
+
+exports.jawsdb = {
+	username: process.env.JAWSDB_USERNAME,
+	password: process.env.JAWSDB_PASSWORD,
+	database: process.env.JAWSDB_DATABASE,
+	host: process.env.JAWSDB_HOST
+}
